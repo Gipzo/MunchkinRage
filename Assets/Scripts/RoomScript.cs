b9a7ef8d4e999ef5474302764c85b99587e7a0e0
Artem Shambalev <gipzo@mbp-gipzo.local>
2015-01-23 23:33:00 +0300

    Added rooms

---
+++ Assets/Scripts/RoomScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomScript : MonoBehaviour {
+
+	public int Width = 1;
+	public int Height = 1;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
