90b1f806ec150217573e6e8d1b0aa1e9a976583a
Karpuk Artsiom <Stegges@Karpuks-MacBook-Air.local>
2015-01-24 11:10:20 +0300

    Inventory

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+
+	public string GetCurrentWeapon(){
+		return null;
+	}
+
+	public string getBody(){
+		return null;
+	}
+}
